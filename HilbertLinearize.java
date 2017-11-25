4af15deffe0413656a4d0bbb16e5cbd9fabd799a
aviralsomani <33675863+aviralsomani@users.noreply.github.com>
2017-11-25 09:15:08 -0600

    Update HilbertLinearize.java

--- HilbertLinearize.java
+++ HilbertLinearize.java
@@ -4,9 +4,9 @@ public class HilbertLinearize
 {
 	public static void main(String[] args)
 	{
-		Matrix a = new Matrix(8, 8);
+		Matrix a = new Matrix(8, 8); 			//This implementation can handle from 2^2 x 2^2 matrix to a 2^13 x 2^13 matrix
 		int c = 1;
-		for (int i = 0; i < a.numRows(); i++)
+		for (int i = 0; i < a.numRows(); i++)		//Creating a matrix to test the linearize method with
 		{
 			for (int j = 0; j < a.numCols(); j++)
 			{	
@@ -15,31 +15,6 @@ public class HilbertLinearize
 			}
 		}
 		System.out.println(a);
-		long startTime;
 		System.out.println(a.linearize());
-		long endTime;
-		/*String out = "";
-		for (int x = 2; x < 13; x++)
-		{
-			int f = (int)(Math.pow(2, x));
-			a = new Matrix(f, f);
-			c = 1;
-			for (int num = 0; num < 10; num++)
-				{
-				for (int i = 0; i < a.numRows(); i++)
-				{
-					for (int j = 0; j < a.numCols(); j++)
-					{	
-						a.set(i, j, c);
-						c++;
-					}
-				}
-			startTime = System.currentTimeMillis();
-			a.linearize();
-			endTime = System.currentTimeMillis();
-			out = out + "Dimension: " + f + ": \t\tRuntime: " + (endTime - startTime) + "\n";
-			}
-		}
-		System.out.println(out);*/
 	}
 }
